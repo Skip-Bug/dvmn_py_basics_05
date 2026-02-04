85b8afc5c74cce5406d51c036d647d3acdd12ba5
Leonid Stoyko <gribojor@gmail.com>
2026-02-05 00:29:52 +0500

    Initial commit

---
+++ create_npc.py
@@ -0,0 +1,44 @@
+import file_operations
+import random
+import os
+from faker import Faker
+from skills_list import skills
+from runic_letters import letters_mapping
+
+
+MIN_STAT = 3
+MAX_STAT = 18
+NAM_NPC = 10
+
+
+def main():
+    os.makedirs("NPC",exist_ok=True)
+    fake = Faker("ru_RU")
+    runic_skills = []
+    for skill in skills:
+        runic_skill = skill
+        for char, runic_char in letters_mapping.items():
+            runic_skill = runic_skill.replace(char, runic_char)
+        runic_skills.append(runic_skill)
+    for namber in range(NAM_NPC):
+        basic_skills = random.sample(runic_skills, 3)
+        context = {
+          "first_name": fake.first_name(),
+          "last_name": fake.last_name(),
+          "job": fake.job(),
+          "town": fake.city(),
+          "strength": random.randint(MIN_STAT, MAX_STAT),
+          "agility": random.randint(MIN_STAT, MAX_STAT),
+          "endurance": random.randint(MIN_STAT, MAX_STAT),
+          "intelligence": random.randint(MIN_STAT, MAX_STAT),
+          "luck": random.randint(MIN_STAT, MAX_STAT),
+          "skill_1": basic_skills[0],
+          "skill_2": basic_skills[1],
+          "skill_3": basic_skills[2]
+          }
+        output_path = "NPC/result_{}.svg".format(namber + 1)
+        file_operations.render_template("charsheet.svg", output_path, context)
+
+
+if __name__ == "__main__":
+    main()
